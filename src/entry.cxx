bccd8f7ade87c577099effbb32e4faab36ec0d29
Alina Hagan <dhagan@lapw.lancs.ac.uk>
2022-10-18 15:36:51 +0100

    minor changes

--- src/entry.cxx
+++ src/entry.cxx
@@ -1,8 +1,8 @@
-#include <common.hxx>
-#include <var.hxx>
-#include <bounds.hxx>
-#include <cutflow.hxx>
-#include <utils.hxx>
+#include "common.hxx"
+#include "var.hxx"
+#include "bounds.hxx"
+#include "cutflow.hxx"
+#include "utils.hxx"
 
 int entry(){
   exit(0);
